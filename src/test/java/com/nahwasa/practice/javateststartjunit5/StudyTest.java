e3e1b5c0a60a56e238efdca5f67ee839917e5890
nahwasa <nahwasa@gmail.com>
2022-12-24 09:44:27 +0900

    Tag와 Tag를 필터링 하는 방법.

--- src/test/java/com/nahwasa/practice/javateststartjunit5/StudyTest.java
+++ src/test/java/com/nahwasa/practice/javateststartjunit5/StudyTest.java
@@ -9,14 +9,88 @@ import static org.junit.jupiter.api.Assertions.*;
 import static org.junit.jupiter.api.Assumptions.assumeTrue;
 import static org.junit.jupiter.api.Assumptions.assumingThat;
 
+/**
+ * Run을 gradle로 실행 시 Disabled에 써둔 Disabled 이유가 화면상에 출력되지 않고,
+ * 'BUILD SUCCESSFUL in 1s' 등과 같은 그래들 메시지도 보인다.
+ * 테스트 내용만 보고 싶고, Tags에 따라 실행하고 싶다면 JUnit으로 Run 해야 한다.
+ *
+ * JUnit으로 실행 시 Tags에 맞춰 실행하려면 edit configuration에서 JUnit으로 된 run config를 class에서 tags로
+ * 변경해서 tag 설정해둔걸 넣어주면 된다.
+ *
+ * gradle로 실행 시 Tags에 맞춰 실행하려면 build.gradle 맨 아래쪽에 작성한 것 처럼 작성 가능하다.
+ * 저걸로 ./gradlew basicTest 처럼 테스트 가능하므로 CI 서버에서도 테스트 가능.
+ * (예를들어 통합테스트는 CI 서버에서만 돌리고 싶다면 별도로 integrationTest 라고 만들거나 하면 됨)
+ */
 @DisplayNameGeneration(DisplayNameGenerator.ReplaceUnderscores.class)   // 함수명에 언더바 붙은걸 공백으로 변환하는 규칙지정
 @DisplayName("'테스트' 스터디용 Study 클래스는")
 class StudyTest {
 
     @Nested
-    @DisplayName("생성시에")
+    @DisplayName("생성시에 (항상 성공해야 하는 테스트들)")
+    @Tag("basic_test")
     class Study_creation {
+        @Test
+        @DisplayName("초기 상태값(StudyStatus)은 INIT 이어야 한다.")
+        void default_status_is_draft() {
+            Study study = new Study();
+            // 첫번째 인자가 expected, 두번째가 actual 이므로 예상 기대값을 첫 번째 인자로 넣을 것.
+            // 람다식으로 작성하지 않을 시 하나의 String 형태가 아닌 이하와 같은 형태에 대해 매번 String 연산을 하게 되므로
+            // 비효율적임. 람다식으로 작성해야 실패시에만 String 연산이 진행됨. 람다식의 원본은 assertEquals의 기존 3번째 인자인
+            // new Supplier<String>() 임.
+            // 즉 그냥 하나의 String으로 된 메시지면 람다식으로 안적어도 되지만, 연산이 들어가면 람다식으로 작성할 것.
+            assertEquals(StudyStatus.INIT, study.getStatus(), () -> "Study 생성시 초기값은 " + StudyStatus.INIT + " 이여야 함.");
+        }
 
+        @Test
+        @DisplayName("null이 아니어야 한다") // DisplayName 지정 시 함수명으로 표기 안됨
+        void is_not_null() {
+            Study study = new Study();
+            assertNotNull(study);
+        }
+
+        @Test
+        @EnabledOnJre(JRE.JAVA_17)
+        @DisplayName("자바 버전이 17이면 실행한다.")
+        void conditional_test_jupyter_jre_17_check() {
+            Study study = new Study();
+            assertNotNull(study);
+        }
+
+        @Test
+        @DisplayName("스터디 최대 참가 인원은 0보다 커야 한다.")
+        void maximum_limit_is_over_zero() {
+            Study study = new Study();
+            assertTrue(study.getLimit() > 0, () -> "스터디 참가 인원은 0 이상이어야 함.");
+        }
+
+        @Test
+        @DisplayName("원하는 익셉션이 발생해야 한다.")
+        void runtime_exception_is_occur() {
+            Study study = new Study();
+            RuntimeException exception =
+                assertThrows(RuntimeException.class, () -> study.makeException(), () -> "makeException 실행 시 RuntimeException이 발생해야 함.");
+            String message = exception.getMessage();
+            assertEquals(StudyExceptionCode.E001.name(), message, "익셉션 메시지는 E001 이어야 한다.");
+        }
+
+        @Test
+        @DisplayName("1부터 100,000까지의 짝수의 합은 10ms 이내로 획득 가능해야 한다.")
+        void sum_of_1_to_100000_under_10ms() {
+            Study study = new Study();
+            long sum = assertTimeout(   // assertTimeout은 시간 지나도 일단 결과 얻음. assertTimeoutPreemptively는 시간 지나면 멈춤.
+                    Duration.ofMillis(10),
+                    () -> study.sumEvenNumbersUnderN(100000),
+                    "1부터 100000까지의 짝수의 합은 10ms 이내로 획득 가능해야 한다."
+            );
+            assertEquals(2500050000l, sum, "1부터 100,000까지의 짝수의 합은 2,500,050,000 이어야 한다.");
+        }
+    }
+
+
+    @Nested
+    @DisplayName("생성시에 (조건에 따라 동작 안하거나 일부러 실패하게 해둔 테스트)")
+    @Tag("conditional_test")
+    class Study_creation_conditional {
         @Test
         @DisplayName("JAVA_HOME 기준으로 JDK가 corretto-17.* 이어야만 수행한다.")
         void conditional_test_java_home_is_corretto_17() {
@@ -69,40 +143,6 @@ class StudyTest {
         }
 
         @Test
-        @DisplayName("초기 상태값(StudyStatus)은 INIT 이어야 한다.")
-        void default_status_is_draft() {
-            Study study = new Study();
-            // 첫번째 인자가 expected, 두번째가 actual 이므로 예상 기대값을 첫 번째 인자로 넣을 것.
-            // 람다식으로 작성하지 않을 시 하나의 String 형태가 아닌 이하와 같은 형태에 대해 매번 String 연산을 하게 되므로
-            // 비효율적임. 람다식으로 작성해야 실패시에만 String 연산이 진행됨. 람다식의 원본은 assertEquals의 기존 3번째 인자인
-            // new Supplier<String>() 임.
-            // 즉 그냥 하나의 String으로 된 메시지면 람다식으로 안적어도 되지만, 연산이 들어가면 람다식으로 작성할 것.
-            assertEquals(StudyStatus.INIT, study.getStatus(), () -> "Study 생성시 초기값은 " + StudyStatus.INIT + " 이여야 함.");
-        }
-
-        @Test
-        @DisplayName("null이 아니어야 한다") // DisplayName 지정 시 함수명으로 표기 안됨
-        void is_not_null() {
-            Study study = new Study();
-            assertNotNull(study);
-        }
-
-        @Test
-        @EnabledOnJre(JRE.JAVA_17)
-        @DisplayName("자바 버전이 17이면 실행한다.")
-        void conditional_test_jupyter_jre_17_check() {
-            Study study = new Study();
-            assertNotNull(study);
-        }
-
-        @Test
-        @DisplayName("스터디 최대 참가 인원은 0보다 커야 한다.")
-        void maximum_limit_is_over_zero() {
-            Study study = new Study();
-            assertTrue(study.getLimit() > 0, () -> "스터디 참가 인원은 0 이상이어야 함.");
-        }
-
-        @Test
         @Disabled("일부러 실패하게 해둔 테스트이므로 Disabled")
         @DisplayName("한꺼번에 여럿 수행하는 경우 1 - assertAll 미사용 (비추)")
         void assert_all_1() {
@@ -129,65 +169,36 @@ class StudyTest {
         }
 
         @Test
-        @DisplayName("원하는 익셉션이 발생해야 한다.")
-        void runtime_exception_is_occur() {
+        void create_function_name_report() {
+            // DisplayName이 없다면 함수명이 기본 표기. DisplayName이 더 나은듯.
+            // 카멜케이스 보다는 언더바 표기가 더 낫다고 함. DisplayNameGenerator.ReplaceUnderscores과 합쳐지니 더 그럴듯.
             Study study = new Study();
-            RuntimeException exception =
-                assertThrows(RuntimeException.class, () -> study.makeException(), () -> "makeException 실행 시 RuntimeException이 발생해야 함.");
-            String message = exception.getMessage();
-            assertEquals(StudyExceptionCode.E001.name(), message, "익셉션 메시지는 E001 이어야 한다.");
+            assertNotNull(study);
+            System.out.println("createMore");
         }
 
-        @Test
-        @DisplayName("1부터 100,000까지의 짝수의 합은 10ms 이내로 획득 가능해야 한다.")
-        void sum_of_1_to_100000_under_10ms() {
-            Study study = new Study();
-            long sum = assertTimeout(   // assertTimeout은 시간 지나도 일단 결과 얻음. assertTimeoutPreemptively는 시간 지나면 멈춤.
-                    Duration.ofMillis(10),
-                    () -> study.sumEvenNumbersUnderN(100000),
-                    "1부터 100000까지의 짝수의 합은 10ms 이내로 획득 가능해야 한다."
-            );
-            assertEquals(2500050000l, sum, "1부터 100,000까지의 짝수의 합은 2,500,050,000 이어야 한다.");
+        @BeforeAll  // 모든 테스트 시작 전 1번
+        @Tag("before_and_after")
+        static void beforeAll() {
+            System.out.println("beforeAll");
+        }
+
+        @AfterAll   // 모든 테스트 시작 후 1번
+        @Tag("before_and_after")
+        static void afterAll() {
+            System.out.println("afterAll");
+        }
+
+        @BeforeEach // 모든 테스트 시작 직전마다 실행
+        @Tag("before_and_after")
+        void beforeEach() {
+            System.out.println("beforeEach");
+        }
+
+        @AfterEach  // 모든 테스트 시작 직후마다 실행
+        @Tag("before_and_after")
+        void afterEach() {
+            System.out.println("afterEach");
         }
     }
-
-
-
-
-
-    /**
-    @Test
-    @Disabled // 해당 테스트 넘기고 싶을 때 (비추)
-    void create_function_name_report() {
-        // DisplayName이 없다면 함수명이 기본 표기. DisplayName이 더 나은듯.
-        // 카멜케이스 보다는 언더바 표기가 더 낫다고 함. DisplayNameGenerator.ReplaceUnderscores과 합쳐지니 더 그럴듯.
-        Study study = new Study();
-        assertNotNull(study);
-        System.out.println("createMore");
-    }
-
-    @BeforeAll  // 모든 테스트 시작 전 1번
-    @Disabled
-    static void beforeAll() {
-        System.out.println("beforeAll");
-    }
-
-    @AfterAll   // 모든 테스트 시작 후 1번
-    @Disabled
-    static void afterAll() {
-        System.out.println("afterAll");
-    }
-
-    @BeforeEach // 모든 테스트 시작 직전마다 실행
-    @Disabled
-    void beforeEach() {
-        System.out.println("beforeEach");
-    }
-
-    @AfterEach  // 모든 테스트 시작 직후마다 실행
-    @Disabled
-    void afterEach() {
-        System.out.println("afterEach");
-    }
-    */
 }
