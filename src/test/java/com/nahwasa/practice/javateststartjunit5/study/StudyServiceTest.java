d787eb76c05b5018fff1929049a25421f1f2a0a7
nahwasa <nahwasa@gmail.com>
2023-01-27 23:32:17 +0900

    Mockito - 함수에 파라미터로 Mock 객체 정의 방법

--- src/test/java/com/nahwasa/practice/javateststartjunit5/study/StudyServiceTest.java
+++ src/test/java/com/nahwasa/practice/javateststartjunit5/study/StudyServiceTest.java
@@ -10,15 +10,12 @@ import org.mockito.junit.jupiter.MockitoExtension;
 @ExtendWith(MockitoExtension.class)
 class StudyServiceTest {
 
-    @Mock
-    MemberService memberService;
-
-    @Mock
-    StudyRepository studyRepository;
-
-
     @Test
-    void createStudyService() {
+    void createStudyService(@Mock MemberService memberService,
+                            @Mock StudyRepository studyRepository) {
+        // 현재는 MemberService와 StudyRepository의 구혅체가 없으므로 테스트하려면 Mock을 쓰는 수밖에 없음.
+        // 구현체가 있더라도 StudyService쪽의 코드 자체만 테스트하고 싶다면 이렇게 Mock으로 주입해주면 됨.
+        // 테스트 전체에 걸쳐서 쓸때는 직전 커밋처럼 인스턴스 변수로 하고, 이 함수내에서만 쓰려면 이렇게 하면 됨.
         StudyService studyService = new StudyService(memberService, studyRepository);
 
         Assertions.assertThat(studyService).isNotNull();
